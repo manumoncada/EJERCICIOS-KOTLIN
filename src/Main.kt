f760a599c86772d3c9870f7b1105f47d89fb9276
manuel <moncadajuanmanuel25@gmail.com>
2025-02-28 19:33:15 -0500

    actualizacion

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    println("ingrese su dinero inicial")
+    val dineroinicial = readln()
+    val numero =  (1..3).random()
+    println("el numero al azar es el "+ numero)
+
+
+    when (numero){
+
+        1 -> {
+           val dinerofinal = dineroinicial.toInt() * 2
+            if (numero =)
+            println("felicidades su dinero ahora es " + dinerofinal )
+        }
+        2->{
+            val dinerofinal = dineroinicial.toInt() / 2
+                println("lo sentimos, has perdido la mitad de tu dinero, el restante es " + dinerofinal)
+
+        }
+        3->{
+            val dinerofinal = dineroinicial.toInt() * 0
+                println("mala suerte, has perdido todo tu dinero")
+        }
     }
+
+
+
+
 }
