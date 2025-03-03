321f7e4e55820ceb021e6bc0b51d644ebf545351
manuel <moncadajuanmanuel25@gmail.com>
2025-03-03 11:59:45 -0500

    rama

--- src/Main.kt
+++ src/Main.kt
@@ -1,33 +1,4 @@
 
 fun main() {
 
-    println("ingrese su dinero inicial")
-    val dineroinicial = readln()
-    val numero =  (1..3).random()
-    val correr=true
-    println("el numero al azar es el "+ numero)
-
-
-    when (correr){
-
-        1 -> {
-
-           val dinerofinal = dineroinicial.toInt() * 2
-
-            println("felicidades su dinero ahora es " + dinerofinal )
-        }
-        2->{
-            val dinerofinal = dineroinicial.toInt() / 2
-                println("lo sentimos, has perdido la mitad de tu dinero, el restante es " + dinerofinal)
-
-        }
-        3->{
-            val dinerofinal = dineroinicial.toInt() * 0
-                println("mala suerte, has perdido todo tu dinero")
-        }
-    }
-
-
-
-
 }
